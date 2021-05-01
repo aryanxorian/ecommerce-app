0c8fabfb6ce9f9f74c7d119fbdd4b7bb0e24d2b8
Ashish Kumar <ashish.k@mindfiresolutions.com>
2021-05-01 22:33:32 +0530

    Updated entrypoint.

--- .docker/local/php/init/docker-entrypoint.sh
+++ .docker/local/php/init/docker-entrypoint.sh
@@ -4,8 +4,12 @@ set -e
 
 sleep 15
 
+mkdir -p /ecommerce-app/bootstrap/cache
+chmod -R 777 /ecommerce-app/bootstrap/cache
+chown -R www-data:www-data /ecommerce-app/bootstrap/cache
+
 # generate app key.
-php /ecommerce-app/artisan key:generate --env=testing
+#php /ecommerce-app/artisan key:generate --env=testing
 
 # Build cache
 php /ecommerce-app/artisan config:cache --env=testing
