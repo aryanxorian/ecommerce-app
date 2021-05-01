49bac117e29ac52000420402a1d73a3d21875a3f
Ashish Kumar <ashish.k@mindfiresolutions.com>
2021-05-01 19:40:17 +0530

    Added docker local env setup.

---
+++ .docker/local/php/init/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+sleep 15
+
+# generate app key.
+php /ecommerce-app/artisan key:generate --env=testing
+
+# Build cache
+php /ecommerce-app/artisan config:cache --env=testing
+
+chmod -R 777 /ecommerce-app/bootstrap/cache
+chown -R www-data:www-data /ecommerce-app/bootstrap/cache
+chmod -R 777 /ecommerce-app/storage/logs/
+
+php /ecommerce-app/artisan migrate:fresh --env=testing
+
+echo "Migrations executed successfully."
+
+exec "$@"
