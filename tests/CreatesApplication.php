7fcd31b4c071815561a10e5e8df569899860604c
Ashish Kumar <ashish.k@mindfiresolutions.com>
2021-05-01 21:59:22 +0530

    Added continuous integration workflow steps.

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -16,6 +16,7 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+        exec('php artisan migrate:fresh --env=testing');
 
         return $app;
     }
