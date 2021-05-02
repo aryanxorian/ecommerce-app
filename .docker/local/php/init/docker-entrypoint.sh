647077ef9802e2e486d1b6143b5d7caccdfef7c3
Ashish Kumar <ashish.k@mindfiresolutions.com>
2021-05-02 14:39:54 +0530

    Change permissions on storage directory.

--- .docker/local/php/init/docker-entrypoint.sh
+++ .docker/local/php/init/docker-entrypoint.sh
@@ -13,7 +13,8 @@ php /ecommerce-app/artisan config:cache --env=testing
 
 chmod -R 777 /ecommerce-app/bootstrap/cache
 chown -R www-data:www-data /ecommerce-app/bootstrap/cache
-chmod -R 777 /ecommerce-app/storage/logs/
+chmod -R 777 /ecommerce-app/storage/
+chown -R www-data:www-data /ecommerce-app/storage/
 
 php /ecommerce-app/artisan migrate:fresh --env=testing
 
